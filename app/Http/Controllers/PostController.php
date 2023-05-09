d6ee2118a74a9c0ca48a9b35dac7ce45e2db068a
Ritesh Rana <13ritesh2001@gmail.com>
2023-05-09 14:45:05 +0530

    some stuf

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+  
+class PostController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {
+        echo "hello world";
+    }
+}
