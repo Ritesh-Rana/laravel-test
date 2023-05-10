a46793d56e3d8daa52c5611861713e4f7409fc75
Ritesh Rana <13ritesh2001@gmail.com>
2023-05-10 13:57:59 +0530

    border

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,22 +16,22 @@ class PostController extends Controller
         $product = $shopify->getProducts();
         $product=json_decode($product);
 
-        $html = "<table>
+        $html = "<table style='border: 1px solid black;'>
         <thead>
-          <tr>
-            <th>Product ID</th>
-            <th>Image</th>
-            <th>Description</th>
-            <th>Price</th>
+          <tr style='border: 1px solid black;'>
+            <th style='border: 1px solid black;'>Product ID</th>
+            <th style='border: 1px solid black;'>Image</th>
+            <th style='border: 1px solid black;'>Description</th>
+            <th style='border: 1px solid black;'>Price</th>
           </tr>
         </thead>
         <tbody>";
         foreach($product as $p){
             $html.="<tr>
-            <td>".$p->id."</td>
-            <td> <img widht='20' height='30' src=".$p->image->src."></td>
-            <td>".$p->body_html."</td>
-            <td>".$p->id."</td>";
+            <td style='border: 1px solid black;'>".$p->id."</td>
+            <td style='border: 1px solid black;'> <img widht='20' height='100' src=".$p->image->src."></td>
+            <td style='border: 1px solid black;'>".$p->body_html."</td>
+            <td style='border: 1px solid black;'>".$p->id."</td>";
         }
        $html.="</tbody>
       </table>"
