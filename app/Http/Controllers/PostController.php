835d1db0a789839a24057f43d9a9019d9eceb5e7
Ritesh Rana <13ritesh2001@gmail.com>
2023-05-11 17:00:38 +0530

    get and update page content

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,44 +15,25 @@ class PostController extends Controller
    */
   public function index()
   {
-    $shopify = app(Shopify::class);
-    $product = $shopify->getProducts();
-    $product = json_decode($product);
-
-    $html = "<table style='border: 1px solid black;'>
-        <thead>
-          <tr style='border: 1px solid black;'>
-            <th style='border: 1px solid black;'>Product ID</th>
-            <th style='border: 1px solid black;'>Image</th>
-            <th style='border: 1px solid black;'>Description</th>
-            <th style='border: 1px solid black;'>Price</th>
-          </tr>
-        </thead>
-        <tbody>";
-    foreach ($product as $p) {
-      $html .= "<tr>
-            <td style='border: 1px solid black;'>" . $p->id . "</td>
-            <td style='border: 1px solid black;'> <img widht='20' height='100' src=" . $p->image->src . "></td>
-            <td style='border: 1px solid black;'>" . $p->body_html . "</td>
-            <td style='border: 1px solid black;'>" . $p->id . "</td>";
-    }
-    $html .= "</tbody>
-      </table>";
-    echo $html;
+    return view('product');
   }
   public function getOrders()
   {
-    $shopify = app(Shopify::class);
-    $orders = $shopify->getOrders();
-    dd($orders);
+    try {
+      $shopify = app(Shopify::class);
+      $orders = $shopify->getOrders();
+      dd($orders);
+    } catch (Exception $e) {
+      echo $e;
+    }
   }
   public function updateProduct($id)
   {
-    $shopify = app(Shopify::class);
-    $fields = [
-      'title' => 'test tv updated from laravel'
-    ];
     try {
+      $shopify = app(Shopify::class);
+      $fields = [
+        'title' => 'test tv updated from laravel'
+      ];
       $response = $shopify->updateProduct((int)$id, $fields);
       echo "Product Updated Successfully with product id " . $id;
     } catch (Exception $e) {
@@ -61,9 +42,9 @@ class PostController extends Controller
   }
   public function updateMetafield($id)
   {
-    $shopify = app(Shopify::class);
-    $data = ['value'=>'India to updated from laravel'];
     try {
+      $shopify = app(Shopify::class);
+      $data = ['value' => 'India to updated from laravel'];
       dd($shopify->updateMetafield($id, $data));
     } catch (Exception $e) {
       echo "Can't update";
@@ -71,9 +52,9 @@ class PostController extends Controller
   }
   public function getProductMetafields($id)
   {
-    $shopify = app(Shopify::class);
-    $data = [];
     try {
+      $shopify = app(Shopify::class);
+      $data = [];
       dd($shopify->getProductMetafields($id, $data));
     } catch (Exception $e) {
       echo $e;
@@ -81,16 +62,17 @@ class PostController extends Controller
   }
   public function orderFulfilment()
   {
-    $shopify = app(Shopify::class);
-    $params = [
-      'location_id'=>80874275106,
-      "status" => 1,
-      "shipment_status" => 1,
-      "tracking_number" => 1213123
-    ];
+    try {
+      $shopify = app(Shopify::class);
+      $params = [
+        'location_id' => 80874275106,
+        "status" => 1,
+        "shipment_status" => 1,
+        "tracking_number" => 1213123
+      ];
 
-    //full fill order by this method and pass required params
-    /*
+      //full fill order by this method and pass required params
+      /*
     $params = [
         "id" => 4714436821281
         "order_id" => 5293576814881
@@ -112,28 +94,78 @@ class PostController extends Controller
         "admin_graphql_api_id" => "gid://shopify/Fulfillment/4714436821281"
       ]
     */
-    // $o = $shopify->createOrderFulfillment(5293576814881, $params);
-    // dd($o);
+      // $o = $shopify->createOrderFulfillment(5293576814881, $params);
+      // dd($o);
 
-    // get counts
-    // $o = $shopify->getOrderFulfillmentsCount(5293576814881, $params);
-    // dd($o);
-    
-    // get details of order fulfil and fulfilment id
-    $o = $shopify->getOrderFulfillments(5293576814881,[]);
-    dd($o);
+      // get counts
+      // $o = $shopify->getOrderFulfillmentsCount(5293576814881, $params);
+      // dd($o);
 
-    // update order fulfil
-    // $o = $shopify->updateOrderFulfillment(5293576814881,4714436821281,$params );
-    // dd($o);
-    
+      // get details of order fulfil and fulfilment id
+      $o = $shopify->getOrderFulfillments(5293576814881, []);
+      dd($o);
 
-    //  $o = $shopify->cancelOrderFulfillment(5293576814881,4714436821281 );
-    //  dd($o);
-
-    //  $o = $shopify->getAssignedFulfillmentOrders('cancellation_requested',[80874275105] );
-    //  dd($o);
+      // update order fulfil
+      // $o = $shopify->updateOrderFulfillment(5293576814881,4714436821281,$params );
+      // dd($o);
 
 
+      //  $o = $shopify->cancelOrderFulfillment(5293576814881,4714436821281 );
+      //  dd($o);
+
+      //  $o = $shopify->getAssignedFulfillmentOrders('cancellation_requested',[80874275105] );
+      //  dd($o);
+    } catch (Exception $e) {
+      echo $e;
+    }
+  }
+  public function getBlogs()
+  {
+    try {
+      $shopify = app(Shopify::class);
+
+      // get all blogs
+      // $blogs=$shopify->getBlogs([]);
+      // dd($blogs);
+
+
+      //update blogs
+      /*
+     array:11 [▼
+        "id" => 96841957665
+        "handle" => "news"
+        "title" => "News"
+        "updated_at" => "2023-05-11T06:48:11-04:00"
+        "commentable" => "no"
+        "feedburner" => null
+        "feedburner_location" => null
+        "created_at" => "2023-04-11T07:14:16-04:00"
+        "template_suffix" => null
+        "tags" => ""
+        "admin_graphql_api_id" => "gid://shopify/OnlineStoreBlog/96841957665"
+      ]
+    */
+      $data = ['title' => 'NewsTo', 'commentable' => 'no'];
+      // $update = $shopify->updateBlog(96841957665,$data);
+      // dd($update);
+
+      //get articles
+      // $article = $shopify->getArticles();
+      // dd($article);
+
+      // get page collection
+      // $pages = $shopify->getPages([]);
+      // dd($pages);
+
+      $pagedata = [
+        'body_html' => '<h1>Ritesh rana</h1>',
+        'author' => 'Ritesh Rana ji'
+      ];
+      // update page content
+      $pages = $shopify->updatePage(115645153569, $pagedata);
+      dd($pages);
+    } catch (Exception $e) {
+      echo $e;
+    }
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::get('/updateproduct/{id}', [PostController::class,'updateProduct']);
 Route::get('/updatemeta/{id}', [PostController::class,'updateMetafield']);
 Route::get('/getmeta/{id}', [PostController::class,'getProductMetafields']);
 Route::get('/orderfulfilment/{id}', [PostController::class,'orderFulfilment']);
+Route::get('/getblogs', [PostController::class,'getBlogs']);
 
