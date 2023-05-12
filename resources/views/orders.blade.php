427d4c363221297e015b2714b8a581dfdf42c698
Ritesh Rana <13ritesh2001@gmail.com>
2023-05-12 12:02:21 +0530

    table for order

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -11,10 +11,51 @@
 </head>
 <body>
     <h1 class="center">Orders</h1>
-    <pre>
-    <?php 
-        print_r($orders);
-    ?>
-    </pre>
+    <style>
+    .order-table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    .order-table th,
+    .order-table td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .order-table thead th {
+        background-color: #f2f2f2;
+    }
+
+    .order-table tbody tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+</style>
+
+<table class="order-table">
+    <thead>
+        <tr>
+            <th>Order ID</th>
+            <th>Customer Email</th>
+            <th>Created At</th>
+            <th>Total Price</th>
+            <th>Payment Gateway</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($orders as $order)
+            <tr>
+                <td>{{ $order->order_number }}</td>
+                <td>{{ $order->customer->email }}</td>
+                <td>{{ $order->created_at }}</td>
+                <td>{{ $order->total_price }}</td>
+                <td>{{ $order->gateway }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
 </body>
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,22 +1,20 @@
-<?php
-
-    $html = "<table style='border: 1px solid black;'>
-        <thead>
-          <tr style='border: 1px solid black;'>
+<table style='border: 1px solid black;'>
+    <thead>
+        <tr style='border: 1px solid black;'>
             <th style='border: 1px solid black;'>Product ID</th>
             <th style='border: 1px solid black;'>Image</th>
             <th style='border: 1px solid black;'>Description</th>
             <th style='border: 1px solid black;'>Price</th>
-          </tr>
-        </thead>
-        <tbody>";
-    foreach ($product as $p) {
-      $html .= "<tr>
-            <td style='border: 1px solid black;'>" . $p->id . "</td>
-            <td style='border: 1px solid black;'> <img widht='20' height='100' src=" . $p->image->src . "></td>
-            <td style='border: 1px solid black;'>" . $p->body_html . "</td>
-            <td style='border: 1px solid black;'>" . $p->id . "</td>";
-    }
-    $html .= "</tbody>
-      </table>";
-    echo $html;
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($product as $p)
+        <tr>
+            <td style='border: 1px solid black;'>{{ $p->id }}</td>
+            <td style='border: 1px solid black;'> <img width='20' height='100' src="{{ $p->image->src }}"></td>
+            <td style='border: 1px solid black;'>{{ $p->body_html }}</td>
+            <td style='border: 1px solid black;'>{{ $p->id }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
