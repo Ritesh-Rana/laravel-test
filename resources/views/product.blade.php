fc120f31a0a1d1574f512937ad0d0494ec24bfa1
Ritesh Rana <13ritesh2001@gmail.com>
2023-05-12 12:07:24 +0530

    added css to table

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,19 +1,40 @@
-<table style='border: 1px solid black;'>
+<style>
+    .product-table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    .product-table th,
+    .product-table td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .product-table thead th {
+        background-color: #f2f2f2;
+    }
+
+    .product-table tbody tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+</style>
+<table class="product-table">
     <thead>
-        <tr style='border: 1px solid black;'>
-            <th style='border: 1px solid black;'>Product ID</th>
-            <th style='border: 1px solid black;'>Image</th>
-            <th style='border: 1px solid black;'>Description</th>
-            <th style='border: 1px solid black;'>Price</th>
+        <tr>
+            <th>Product ID</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Price</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($product as $p)
         <tr>
-            <td style='border: 1px solid black;'>{{ $p->id }}</td>
-            <td style='border: 1px solid black;'> <img width='20' height='100' src="{{ $p->image->src }}"></td>
-            <td style='border: 1px solid black;'>{{ $p->body_html }}</td>
-            <td style='border: 1px solid black;'>{{ $p->id }}</td>
+            <td>{{ $p->id }}</td>
+            <td><img width="200" height="100" src="{{ $p->image->src }}"></td>
+            <td>{{ $p->body_html }}</td>
+            <td>{{ $p->id }}</td>
         </tr>
         @endforeach
     </tbody>
